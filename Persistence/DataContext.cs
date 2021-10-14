f29eed9d9e3e19950668d4fcf9728898d1a1c0f0
Marisa Norton <marisanorton@mac-mini.home>
2021-10-13 19:10:07 -0500

    finished activity 3

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,21 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Persistence
+{
+        public class DataContext: DbContext
+        {
+            public DbSet<Value> Values { get; set; }
+
+            public DataContext(DbContextOptions options) : base(options)
+            {
+
+            }
+        }
+}
