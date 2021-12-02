5ff0500660f45b511f83f484d789cc418f8b03c5
Marisa Norton <marisanorton@mac-mini.home>
2021-12-01 18:58:14 -0600

    added create endpoint

--- API/Controllers/PostsController.cs
+++ API/Controllers/PostsController.cs
@@ -28,14 +28,42 @@ namespace API.Controllers
         }
         public async Task<ActionResult<List<Post>>> List() 
         {
-            return this.context.Posts.ToList();
+            return await this.mediator.Send(new List.Query());
         }
 
         [HttpGet]
+        public ActionResult<List<Post>> GetActionResult()
+        {
+            return this.context.Posts.ToList();
+        }
 
+        [HttpGet("{id}")]
         public ActionResult<Post> GetById(Guid id)
         {
             return this.context.Posts.Find(id);
         }
+
+        [HttpPost]
+        public ActionResult<Post> Create([FromBody]Post request)
+        {
+            var post = new Post
+            {
+                Id = request.Id,
+                Title = request.Title,
+                Body = request.Body,
+                Date = request.Date
+            };
+
+            context.Posts.Add(post);
+            var success = context.SaveChanges() > 0;
+
+            if (success)
+            {
+                return post;
+            }
+            
+            throw new Exception("Error creating post.");
+        }
+
     }
 }
